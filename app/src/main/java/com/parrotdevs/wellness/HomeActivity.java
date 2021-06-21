acbbcb6a489ed46b17f95dc6f6351b0a47246c25
Sok4h <mrsokaxd@gmail.com>
2021-06-21 17:33:07 -0500

    open nav drawer when clicking button

--- app/src/main/java/com/parrotdevs/wellness/HomeActivity.java
+++ app/src/main/java/com/parrotdevs/wellness/HomeActivity.java
@@ -1,14 +1,27 @@
 package com.parrotdevs.wellness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    DrawerLayout drawerlayout;
+    ImageView btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        drawerlayout = findViewById(R.id.drawer_layout);
+        btnMenu = findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(v->{
+
+        drawerlayout.openDrawer(GravityCompat.START);
+        });
     }
 }
