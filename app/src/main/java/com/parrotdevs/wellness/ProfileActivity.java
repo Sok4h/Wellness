f7bdc00b89d192fc8efc7b5048507dddd97b81bb
danni3231 <danni3231@hotmail.com>
2021-06-21 19:07:37 -0500

    added ProfileEditActivity

--- app/src/main/java/com/parrotdevs/wellness/ProfileActivity.java
+++ app/src/main/java/com/parrotdevs/wellness/ProfileActivity.java
@@ -2,13 +2,24 @@ package com.parrotdevs.wellness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button btnEditProfile ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        btnEditProfile =  findViewById(R.id.btnEditProfile);
+
+        btnEditProfile.setOnClickListener( v -> {
+           Intent intent = new Intent(this,EditProfileActivity.class);
+           startActivity(intent);
+        });
     }
 }
