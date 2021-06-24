5d3e13a53e30fe6dfe17ef5113f0db99361a01ea
Sok4h <mrsokaxd@gmail.com>
2021-06-24 18:44:54 -0500

    redirecting to activity depending if a user is logged in

--- app/src/main/java/com/parrotdevs/wellness/activities/MainActivity.java
+++ app/src/main/java/com/parrotdevs/wellness/activities/MainActivity.java
@@ -6,16 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.parrotdevs.wellness.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ChangeActivity();
+
+        auth = FirebaseAuth.getInstance();
+
+        auth.addAuthStateListener(firebaseAuth -> {
+
+            user= firebaseAuth.getCurrentUser();
+            ChangeActivity();
+
+        });
+
     }
+
+
 
     private void ChangeActivity() {
 
@@ -23,9 +38,19 @@ public class MainActivity extends AppCompatActivity {
 
         handler.postDelayed(() -> {
 
-            Intent intent = new Intent(this, SplashActivity.class);
-            startActivity(intent);
-            finish();
+            if(user != null){
+
+                Intent intent = new Intent(this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+            else{
+
+                Intent intent = new Intent(this, SplashActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
 
         }, 2000);
     }
