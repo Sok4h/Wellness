f0ea8945b7b5e00936693523ac34ef751bb16529
Sok4h <mrsokaxd@gmail.com>
2021-06-29 17:00:20 -0500

    notifications

---
+++ app/src/main/java/com/parrotdevs/wellness/utils/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.parrotdevs.wellness.utils;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+
+}
